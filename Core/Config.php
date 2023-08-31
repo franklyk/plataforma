e9b7ba3000dabff547bfa054f935bfdf0386715f
Franklin da Silva Batista <frsbatist@gmail.com>
2023-08-30 21:29:55 -0300

    Início do projeto
    Primeiro atualização após o início onde grande quantidade de pastas já foram criadas. A partir daqui seguiremos.
    21:29
    30/08/2023
    (Obs: Quando foi criado o projeto a data na máquina estava errada. Por isso a impressão de erro na data.

---
+++ Core/Config.php
@@ -0,0 +1,42 @@
+<?php
+
+    //Iniciando todas as seções da aplicação
+    session_start();
+
+    //Configuração da data e hora do servidor
+    date_default_timezone_set('America/São Paulo');
+
+    //Configurções do site
+    $configBase = 'http://localhost/plataforma';
+    $imageSite = 'default.png';
+    $titleSite = 'Store Masters';
+    $description = 'Loja para teste de e-commerce';
+    $mailSite = 'contato@email.com';
+    $phoneSite = '+55(61)123456789';
+    $statusSite = 1; // [0] = Off line / [1] = On line / [2] = Em manutenção
+    $hellobar = 0; //[0] = Desativado / [1] = Ativado
+
+
+    //Configuarções do Author
+    $nameAuthor = 'Franklin Da KLYK / WEB-SITES';
+    $mailAuthor = "mail@email.com";
+    $phoneAuthor = '55 61 993688275';
+
+
+    //Navegação Dinamica do Site
+    $themeSite = 'Themes/Loja';
+    $themePainel = 'Themes/Painel';
+    $themeAdmin = 'Themes/Admin';
+
+    //Configurações do Banco de dados
+
+
+    //Configuraçoes dos perfis de acesso
+    
+    
+    
+    //Configurações de redes sociais
+
+    //Configurações de GUID
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $configbase = 'http://localhost/plataforma/';
+    $configBase = 'http://localhost/plataforma/';
 ?>
 
 
@@ -8,32 +8,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Plataforma</title>
+    <title><?= $titleSite ?></title>
     <meta http-equiv="X-UA Compatible" content="IE=edge">
     <meta name="robots" content="index, follow">
     <meta name="description" content="">
-
-    <meta itemprop="name" content="nome-do site">
-    <meta itemprop="description" content="descricao do site">
-    <meta itemprop="image" content="imagem-do site">
-    <meta itemprop="BASE" content="<?= $configbase ?>">
+    
+    <meta itemprop="name" content="<?= $titleSite ?>">
+    <meta itemprop="description" content="<?= $description ?>">
+    <meta itemprop="image" content="<?= $imageSite ?>">
+    <meta itemprop="BASE" content="<?= $configBase ?>">
     
     <!-- FACEBOOK -->
     <meta property="og:type" content="article-do site">
-    <meta property="og:name" content="nome-do site">
-    <meta property="og:description" content="descricao do site">
-    <meta property="og:image" content="imagem-do site">
-    <meta property="og:BASE" content="<?= $configbase ?>">
-    <meta property="og:sitename" content="titulo-do site">
+    <meta property="og:name" content="<?= $titleSite?>">
+    <meta property="og:description" content="<?= $description ?>">
+    <meta property="og:image" content="<?= $imageSite ?>">
+    <meta property="og:BASE" content="<?= $configBase ?>">
+    <meta property="og:sitename" content="<?= $titleSite ?>">
     <meta property="og:locale" content="pt-br">
 
     <!-- TWITTER -->
     <meta property="twitter:card" content="sumary_large_image">
     <meta property="twitter:domain" content="<?= $configbase ?>">
-    <meta property="twitter:title" content="titulo-do-site">
-    <meta property="twitter:description" content="descricao-do-site">
-    <meta property="twitter:image" content="imagem-do-site">
-    <meta property="twitter:BASE" content="<?= $configbase ?>/image">
+    <meta property="twitter:title" content="<?= $titleSite ?>">
+    <meta property="twitter:description" content="<?= $description ?>">
+    <meta property="twitter:image" content="<?= $imageSite ?>">
+    <meta property="twitter:BASE" content="<?= $configbase ?>/<?= $imageSite ?>">
 
     <!-- FONTAWELSOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.6.3/css/all.min.css" integrity="sha512-f2MWjotY+JCWDlE0+QAshlykvZUtIm35A6RHwfYZPdxKgLJpL8B+VVxjpHJwZDsZaWdyHVhlIHoblFYGkmrbhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -44,10 +44,10 @@
     <link rel="stylesheet" href="https://fonts_googleapis/css2?family=Nunito:wght@300:500&display=swap">
 
     <!-- CSS NATIVE -->
-    <link rel="stylesheet" href="<?= $configbase ?>Views/Css/Views.css">
-    <link rel="stylesheet" href="<?= $configbase ?>Themes/Loja/themes.css">
+    <link rel="stylesheet" href="<?= $configBase ?>Views/Css/Views.css">
+    <link rel="stylesheet" href="<?= $configBase ?>Themes/Loja/themes.css">
 
-    <script src="<?= $configbase ?>/Views/js/jquery.js"></script>
+    <script src="<?= $configBase ?>/Views/js/jquery.js"></script>
 
 </head>
     <body>
@@ -55,13 +55,13 @@
             require 'Core/Routers.php';
         ?>
 
-        <script src="<?= $configbase ?>/Views/js/maskinput.js"></script>
-        <script src="<?= $configbase ?>/Views/js/jquery-money.js"></script>
-        <script src="<?= $configbase ?>/Views/js/jquery-form.js"></script>
-        <script src="<?= $configbase ?>/Views/js/Views.js"></script>
+        <script src="<?= $configBase ?>/Views/js/maskinput.js"></script>
+        <script src="<?= $configBase ?>/Views/js/jquery-money.js"></script>
+        <script src="<?= $configBase ?>/Views/js/jquery-form.js"></script>
+        <script src="<?= $configBase ?>/Views/js/Views.js"></script>
 
-        <script src="<?= $configbase ?>Ajax/Ajax.js"></script>
-        <script src="<?= $configbase ?>Themes/Loja/themes.js"></script>
+        <script src="<?= $configBase ?>Ajax/Ajax.js"></script>
+        <script src="<?= $configBase ?>Themes/Loja/themes.js"></script>
 
     </body>
 </html>
