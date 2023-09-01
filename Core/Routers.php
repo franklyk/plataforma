9a2e5625d69769748cd9902584c1355ab5fafd64
Franklin da Silva Batista <frsbatist@gmail.com>
2023-09-01 01:47:09 -0300

    .htacces  Routers  error404
    Criação de Rotas de navegação
    Configuração de .htaccess
    Definição das paginas Adm Loja e Painel
    01:47
    01/09/2023

---
+++ Core/Routers.php
@@ -0,0 +1,25 @@
+<?php
+    $configUrl = explode('/', strip_tags(filter_input(INPUT_GET, 'route', FILTER_DEFAULT)));
+    $configUrl[0] = (!empty($configUrl[0]) ? $configUrl[0] : 'index');
+
+    if($configUrl[0] == 'admin'){
+        $themes = "Themes/Admin/";
+    }else if($configUrl[0] == 'painel'){
+        $themes = "Themes/Painel/";
+    }else{
+        $themes = "Themes/Loja/";
+    }
+    if($configUrl[0] !== 'admin' && $configUrl[0] !== 'painel' && file_exists("{$themes}{$configUrl[0]}.php"))  {
+        //Arquivo
+        require "{$themes}{$configUrl[0]}.php";
+    }elseif(!empty($configUrl[1]) && file_exists("{$themes}{$configUrl[1]}.php") && !is_dir("{$themes}{$configUrl[1]}.php")){
+        //Pasta/Arquivo
+        require "{$themes}{$configUrl[1]}.php";
+
+    }elseif(!empty($configUrl[2]) && file_exists("{$themes}{$configUrl[1]}/{$configUrl[2]}.php") && !is_dir("{$themes}{$configUrl[1]}/{$configUrl[2]}.php")){
+        //Pasta/Subpasta/Arquivo || No caso deste projeto a especificação será //Pasta/Arquivo/Parametro
+        require "{$themes}{$configUrl[1]}/{$configUrl[2]}.php";
+
+    }else{
+        require 'error404.php';
+    }
