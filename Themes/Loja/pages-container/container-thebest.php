5a68442cb59264b9c9bc10ca5e463129abf1a0fe
Franklin da Silva Batista <frsbatist@gmail.com>
2023-09-07 00:33:24 -0300

    00:31 07/09/2023
    reestruturação do código para CSS-GRID-LAYOUT

--- Themes/Loja/pages-container/container-thebest.php
+++ Themes/Loja/pages-container/container-thebest.php
@@ -6,47 +6,45 @@
     <link rel="stylesheet" href="Views/Css/Views.css">
 </head>
 <body>
-    <div class="container_top">
-        <h1 class="font-text-medium font-weight-medium">Produtos em Destaque:</h1>
-        <div class="container_top_info">
-            <ul class="caroussel">
-                <?php for($i=0; $i < 6; $i++): ?>
-                    <li class="img-caroussel">
-                        <div class="divisor2">
-                            <p class="font-weight-medium price_discount radius">10% OFF</p>
-                            <img src="<?= $configBase ?>Images/Products/product.png" title="Imagem do produto: Tênis Adidas Sporting" alt="Imagem do produto: Tênis Adidas Sporting">
-                        </div>
-                        <div class="divisor2 product_desciption">
+    <h1 class="font-text-max font-weight-max color-blue-dark text-margin title-folder">Produtos em Destaque:</h1>
 
-                            <h2 class=" m-text-center font-weight-medium font-text-min">Tênis Adidas Sporting</h2>
+    <div class="container_top ">
 
-                            <p class="text-left m-text-center font-weight-min">
+        <?php for($i=0; $i < 6; $i++): ?>
 
-                                <span class="price_old radius font-text-sub price_discount"><s> R$ 450,00</s></span>
+        <div class="exposed bgcolor-white-light">
 
-                                <span class="price_off radius font-text-medium font-weight-medium new_price"> R$ 320,00</span>
+            <img class="product_img" src="<?php $configBase ?>Images/Products/product.png" title="Imagem do produto: Tênis Adidas Sporting" alt="Imagem do produto: Tênis Adidas Sporting">
 
-                            </p>
+            <h2 class="product_title">
+                Tênis Adidas Sporting
+            </h2>
 
-                            <p class="text-left m-text-center font-text-sub description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam iste facere aperiam labore distinctio nemo aspernatur.</p>
+            <p class="product_description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam iste facere aperiam labore distinctio nemo aspernatur.
+            </p>
 
-                            <p class="text-center font-weight-min font-text-sub actions">
+            <span class="discount_off color-red font-weight-max">
+                10% OFF
+            </span>
 
-                                <a href="<?= $configBase ?>details/<?= $i ?>" title="Visualize os detalhes deste produto."
-                                class="btn_edit radius"><i class="fa fa-images"></i>
-                                </a>
+            <p class="prices">
+                <span class="old_price color-red font-text-sub"><s> R$ 450,00</s></span>
 
-                                <a href="#" title="Adicione este produto ao carrinho."
-                                class="btn_new radius open-buy" data-id="<?= $i ?>">
-                                    <i class="fa fa-shopping-cart"></i>
-                                </a>
-                            </p>
-                        </div>
-                    </li>
-                <?php endfor; ?>
-            </ul>
-            <div class="clear"></div>
+                <span class="new_price font-weight-max font-text-extra color-green-dark"> R$ 320,00</span>
+            </p>
+
+            <p class="btn_actions">
+                <a href="#" title="Visualize os detalhes deste produto." class="btn_datails bgcolor-blue color-white radius">
+                    <i class="fa fa-images"></i>
+                </a>
+                <a class="btn_cart bgcolor-green color-white radius" href="#" title="Adicione este produto ao carrinho.">
+                    <i class="fa fa-shopping-cart"></i>
+                </a>
+            </p>
         </div>
+        <?php endfor; ?>
+
     </div>
 </body>
 </html>
--- Themes/Loja/pages-container/container-top.php
+++ Themes/Loja/pages-container/container-top.php
@@ -6,84 +6,76 @@
     <link rel="stylesheet" href="Views/Css/Views.css">
 </head>
 <body>
-    <div class="container_top">
-        <div class="container_top_info">
-            <ul class="container_list">
-                <li>
-                    <p class="font-text-max ">
-                        <i class="fa fa-truck"></i>
-                    </p>
-                    <p class="text-center font-text-sub font-weight-medium">Frete Gratuito</p>
-                    <p class="text-center font-text-sub ">Compras a partir de R$200,00</p>
-                </li>
-                <li>
-                <p class="text-center font-text-max">
-                        <i class="fa fa-credit-card"></i>
-                    </p>
-                    </p>
-                    <p class="text-center font-text-sub font-weight-medium">Pagamento no Cartão</p>
-                    <p class="text-center font-text-sub ">Pague no Cartão de Crédito em até 12x</p>
-                </li>
-                <li>
-                <p class="text-center font-text-max">
-                        <i class="fa fa-gift"></i>
-                    </p>
-                    </p>
-                    <p class="text-center font-text-sub font-weight-medium">Ganhe Descontos nas Suas Próximas Compras</p>
-                </li>
-                <li>
-                <p class="text-center font-text-max">
-                        <i class="fa fa-phone"></i>
-                    </p>
-                    </p>
-                    <p class="text-center font-text-sub font-weight-medium">
-                        <span class="none"></span>Central de Atendimento
-                    </p>
-                    <p class="text-center font-text-sub ">Todos os Dias 24h/dia</p>
-                </li>
-            </ul>
-            <div class="clear"></div>
+    <div class="container_top_info">
+        <div class="bgcolor-white-light radius color-orange">
+            <p class="font-text-extra ">
+                <i class="fa fa-truck font-text-max "></i>
+            <p class="text-center font-text-medium font-weight-medium color-orange">Frete Gratuito</p>
+            <p class="text-center font-text-medium color-orange">Compras a partir de R$200,00</p>
         </div>
-        <h1 class="font-text-medium font-weight-medium">Mais vendidos da semana:</h1>
-        <div class="container_top_info">
-            <ul class="caroussel">
-                <?php for($i=0; $i < 6; $i++): ?>
-                    <li class="img-caroussel">
-                        <div class="divisor2">
-                            <p class="font-weight-medium price_discount radius">10% OFF</p>
-                            <img src="<?= $configBase ?>Images/Products/product.png" title="Imagem do produto: Tênis Adidas Sporting" alt="Imagem do produto: Tênis Adidas Sporting">
-                        </div>
-                        <div class="divisor2 product_desciption">
-
-                            <h2 class=" m-text-center font-weight-medium font-text-min">Tênis Adidas Sporting</h2>
-
-                            <p class="text-left m-text-center font-weight-min">
-
-                                <span class="price_old radius font-text-sub price_discount"><s> R$ 450,00</s></span>
-
-                                <span class="price_off radius font-text-medium font-weight-medium new_price"> R$ 320,00</span>
-
-                            </p>
-
-                            <p class="text-left m-text-center font-text-sub description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam iste facere aperiam labore distinctio nemo aspernatur.</p>
-
-                            <p class="text-center font-weight-min font-text-sub actions">
-
-                                <a href="<?= $configBase ?>details/<?= $i ?>" title="Visualize os detalhes deste produto."
-                                class="btn_edit radius"><i class="fa fa-images"></i>
-                                </a>
-
-                                <a href="#" title="Adicione este produto ao carrinho."
-                                class="btn_new radius open-buy" data-id="<?= $i ?>">
-                                    <i class="fa fa-shopping-cart"></i>
-                                </a>
-                            </p>
-                        </div>
-                    </li>
-                <?php endfor; ?>
-            </ul>
-            <div class="clear"></div>
+        <div class="bgcolor-white-light color-blue radius">
+            <p class="text-center font-text-extra">
+                    <i class="fa fa-credit-card font-text-max"></i>
+            </p>
+            <p class="text-center font-text-medium font-weight-medium">Pagamento no Cartão</p>
+            <p class="text-center font-text-medium ">Pague no Cartão de Crédito em até 12x</p>
         </div>
+        <div class="bgcolor-white-light color-red radius">
+            <p class="text-center font-text-medium">
+                    <i class="fa fa-gift font-text-max "></i>
+            </p>
+            <p class="text-center font-text-medium font-weight-medium">Ganhe Descontos nas Suas Próximas Compras</p>
+        </div>
+        <div class="bgcolor-white-light color-green radius">
+            <p class="text-center font-text-medium">
+                <i class="fa fa-phone font-text-max "></i>
+            </p>
+            <p class="text-center font-text-medium font-weight-medium">
+                <span class="none"></span>Central de Atendimento
+            </p>
+            <p class="text-center font-text-medium ">Todos os Dias 24h/dia</p>
+        </div>
+    </div>
+
+    <h1 class="font-text-max font-weight-max color-blue-dark text-margin title-folder ">Mais vendidos da semana:</h1>
+    
+    <div class="container_top ">
+
+        <?php for($i=0; $i < 6; $i++): ?>
+
+        <div class="exposed bgcolor-white-light">
+
+            <img class="product_img" src="<?php $configBase ?>Images/Products/product.png" title="Imagem do produto: Tênis Adidas Sporting" alt="Imagem do produto: Tênis Adidas Sporting">
+
+            <h2 class="product_title">
+                Tênis Adidas Sporting
+            </h2>
+
+            <p class="product_description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quam iste facere aperiam labore distinctio nemo aspernatur.
+            </p>
+
+            <span class="discount_off color-red font-weight-max">
+                10% OFF
+            </span>
+
+            <p class="prices">
+                <span class="old_price color-red font-text-sub"><s> R$ 450,00</s></span>
+
+                <span class="new_price font-weight-max font-text-extra color-green-dark"> R$ 320,00</span>
+            </p>
+
+            <p class="btn_actions">
+                <a href="#" title="Visualize os detalhes deste produto." class="btn_datails bgcolor-blue color-white radius">
+                    <i class="fa fa-images"></i>
+                </a>
+                <a class="btn_cart bgcolor-green color-white radius" href="#" title="Adicione este produto ao carrinho.">
+                    <i class="fa fa-shopping-cart"></i>
+                </a>
+            </p>
+        </div>
+        <?php endfor; ?>
+
     </div>
 </body>
 </html>
